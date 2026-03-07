a6268550aa5e846e80ede7454ec5abaea18f853a
yawlee64-ui <yawlee64@gmail.com>
2026-03-07 23:15:28 +0000

    labwork4

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,26 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "../src/app.js";
+import { jest } from "@jest/globals";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+jest.setTimeout(30000);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Load test environment variables
+dotenv.config({ path: `${__dirname}/../.env.test` });
+
+// Connect to MongoDB before all tests
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  // Clear all collections
+  await mongoose.connection.db.dropDatabase();
+});
+
+// Close connection after all tests
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
 export default app;
